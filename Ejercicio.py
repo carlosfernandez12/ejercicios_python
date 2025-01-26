840779ae2c8e2b2a9ad081969a6e34e652584c91
gamers retos <c.fernandez.c069@edu.utc.mx>
2025-01-25 18:45:11 -0600

    ejercicio calculadora

---
+++ Ejercicio.py
@@ -0,0 +1,10 @@
+#crear una calculadora
+numero1 = int(input("DAME UN NUMERO: "))
+numero2 = int(input("DAME EL SIGUIENTE NUMERO: "))
+
+resultado = numero1 + numero2
+resultado2 = numero1 - numero2
+resultado3 = numero1 * numero2
+resultado4 = numero1 / numero2
+
+print("Suma:", resultado, "Resta:", resultado2, "Multiplicación:", resultado3, "División:", resultado4)
